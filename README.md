e8644b5f1f55c31cb203982a9dd0648e1eea644a
niklaus <niklaus.lee@gmail.com>
2016-05-29 14:01:21 +0900

    Change name to "merge generalizations"

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-StarUML 2 Extension for Arranging Specializations
-=================================================
+StarUML 2 Extension for Merging Generalizations
+===============================================
 
-This extension allows to arrange specialization views quickly into a rectilinear form as shown in the below figures.
+This extension allows to merge generalization views quickly into a rectilinear form as shown in the below figures.
 
 ![Screenshot1](https://github.com/niklauslee/staruml-arrange-specializations/blob/master/captures/screenshot1.png?raw=true)
 ![Screenshot2](https://github.com/niklauslee/staruml-arrange-specializations/blob/master/captures/screenshot2.png?raw=true)
 
 ## Installation
 
-Open Extension Manager *(Tools > Extension Manager)* and select *Registry* Tab and enter "arrange" in search box. You can find this extension and then click **install** button.
+Open Extension Manager *(Tools > Extension Manager)* and select *Registry* Tab and enter "merge" in search box. You can find this extension and then click **install** button.
 
 ## Usage
 
-To arrange specializations, select a Class (Superclass) first and then select **Tools > Arrange Specializations (Alt+Ctrl+S)** or click **Arrange Specializations** icon in Toolbar.
+To merge generalizations, select a Class (Superclass) first and then select **Tools > Merge Generalizations (Alt+Ctrl+S)** or click **Merge Generalizations** icon in Toolbar.
 
 ---
 
--- main.js
+++ main.js
@@ -38,53 +38,53 @@ define(function (require, exports, module) {
         DiagramManager   = app.getModule("diagrams/DiagramManager"),
         SelectionManager = app.getModule("engine/SelectionManager"),
         Toast            = app.getModule("ui/Toast");
-    
-    // Command ID
-    var CMD_ARRANGE_SPECIALIZATIONS = "tools.arrange-specializations";
-    
-    // Toolbar Button
-    var $button = $("<a id='toolbar-arrange-specializations' href='#' title='Arrange Specializations'></a>");
 
-    
+    // Command ID
+    var CMD_MERGE_GENERALIZATIONS = "tools.merge-generalizations";
+
+    // Toolbar Button
+    var $button = $("<a id='toolbar-merge-generalizations' href='#' title='Merge Generalizations'></a>");
+
+
     /**
-     * Return specialization views of a given view.
+     * Return generalization views of a given view.
      */
-    function getSpecializations(selected) {
+    function getGeneralizations(selected) {
         var edges = Repository.getEdgeViewsOf(selected);
         var results = _.filter(edges, function (e) {
             return (e instanceof type.UMLGeneralizationView) && (e.head === selected);
         });
         return results;
     }
-        
+
     /**
-     * Arrange specializations
+     * Merge generalizations
      */
-    function arrange() {
+    function merge() {
         // Get a selected view
         var views  = SelectionManager.getSelectedViews();
         if (views.length < 1) {
             Toast.info("Nothing selected.");
             return;
         }
-        
+
         // Get specialization views
         var selected  = views[0];
-        var specializations = getSpecializations(selected);
-        if (specializations.length < 1) {
-            Toast.info("No specializations to arrange.");
+        var generalizations = getGeneralizations(selected);
+        if (generalizations.length < 1) {
+            Toast.info("No generalizations to merge.");
             return;
         }
 
-        // Compute coordinates for specializations
-        var topLine = _.min(_.map(specializations, function (e) { return e.tail.top; })),
+        // Compute coordinates for generalizations
+        var topLine = _.min(_.map(generalizations, function (e) { return e.tail.top; })),
             x       = Math.round((selected.left + selected.getRight()) / 2),
             y1      = Math.round(selected.getBottom()),
             y2      = Math.round((topLine + selected.getBottom() + 15) / 2);
 
         // Make a command and execute
-        OperationBuilder.begin('arrange specializations');
-        specializations.forEach(function (view) {
+        OperationBuilder.begin('merge generalizations');
+        generalizations.forEach(function (view) {
             if (view.lineStyle !== Core.LS_RECTILINEAR) {
                 OperationBuilder.fieldAssign(view, 'lineStyle', Core.LS_RECTILINEAR);
             }
@@ -101,29 +101,29 @@ define(function (require, exports, module) {
         OperationBuilder.end();
         Repository.doOperation(OperationBuilder.getOperation());
     }
-    
+
     /**
      * Initialize Extension
      */
     function init() {
         // Load our stylesheet
         ExtensionUtils.loadStyleSheet(module, "styles.less");
-        
+
         // Toolbar Button
         $("#toolbar .buttons").append($button);
         $button.click(function () {
-            CommandManager.execute(CMD_ARRANGE_SPECIALIZATIONS);
+            CommandManager.execute(CMD_MERGE_GENERALIZATIONS);
         });
-        
+
         // Register Commands
-        CommandManager.register("Arrange Specializations", CMD_ARRANGE_SPECIALIZATIONS, arrange);
+        CommandManager.register("Merge Generalizations", CMD_MERGE_GENERALIZATIONS, merge);
 
         // Setup Menus (Add in Tools)
         var menu = MenuManager.getMenu(Commands.TOOLS);
-        menu.addMenuItem(CMD_ARRANGE_SPECIALIZATIONS, ["Ctrl-Alt-S"]);
-        
+        menu.addMenuItem(CMD_MERGE_GENERALIZATIONS, ["Ctrl-Alt-S"]);
+
     }
-    
+
     init();
-    
+
 });
